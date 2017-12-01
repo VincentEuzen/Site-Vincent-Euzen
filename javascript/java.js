aab073da3b757802350296b9f5bf9c6eda5516a6
Vincent Euzen <vincenteuzen@gmail.com>
2017-12-01 15:30:10 +0100

    test annimation

--- javascript/java.js
+++ javascript/java.js
@@ -8,15 +8,24 @@ $(document).ready(function() {
     // } else {
     //   $(this).removeClass('cercle-middle').addClass('cercle-when-clicked');
     // }
-
-
     $('.left').toggleClass('all-screen-width');
     $('#hidden-text-menu').toggleClass("hidden-for-js");
     $('#hidden-text-menu').toggleClass("show fadeInNew");
     $('.texte').toggleClass("hidden-for-js");
     $('.texte').toggleClass("show");
-});
     });
+
+
+    $('.ancre-about').on('click', function() { // Au clic sur un élément
+        var page = $(this).attr('href'); // Page cible
+        var speed = 750; // Durée de l'animation (en ms)
+        $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+        return false;
+      });
+
+
+
+});
 
 
 
