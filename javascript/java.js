61a21f4ae27e1d760f0ce0750a22e02ae7a6ed08
Vincent Euzen <vincenteuzen@gmail.com>
2017-12-01 14:21:01 +0100

    add css animations

--- javascript/java.js
+++ javascript/java.js
@@ -3,12 +3,18 @@ $(document).ready(function() {
 
   $('.cercle-middle').on('click', function() {
     $(this).toggleClass('cercle-when-clicked');
+    // if ($(this).hasClass('cercle-when-clicked')) {
+    //   $(this).removeClass('cercle-when-clicked').removeClass('cercle-middle').addClass('cercle-when-clicked-return');
+    // } else {
+    //   $(this).removeClass('cercle-middle').addClass('cercle-when-clicked');
+    // }
+
+
     $('.left').toggleClass('all-screen-width');
     $('#hidden-text-menu').toggleClass("hidden-for-js");
-    $('#hidden-text-menu').toggleClass("show");
+    $('#hidden-text-menu').toggleClass("show fadeInNew");
     $('.texte').toggleClass("hidden-for-js");
     $('.texte').toggleClass("show");
-
 });
     });
 
