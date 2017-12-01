1425cf4d1a6adab9e394e128f51ced90851c2133
Vincent Euzen <vincenteuzen@gmail.com>
2017-12-01 15:58:14 +0100

    smaller size pic

--- javascript/java.js
+++ javascript/java.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
     });
 
 
-    $('.ancre-about').on('click', function() { // Au clic sur un élément
+    $('.js-scrollTo').on('click', function() { // Au clic sur un élément
         var page = $(this).attr('href'); // Page cible
         var speed = 750; // Durée de l'animation (en ms)
         $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
