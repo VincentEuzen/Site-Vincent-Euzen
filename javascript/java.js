4c177be075155f10ea9f8b1f2c62a1415679314d
Vincent Euzen <vincenteuzen@gmail.com>
2017-12-03 13:17:12 +0100

    Add box test 1

--- javascript/java.js
+++ javascript/java.js
@@ -9,23 +9,48 @@ $(document).ready(function() {
     //   $(this).removeClass('cercle-middle').addClass('cercle-when-clicked');
     // }
     $('.left').toggleClass('all-screen-width');
+    // $('.cercle-middle-fin').toggleClass('cercle-middle-fin-extend');
+    // $('#img-semi-cercle').toggleClass('img-semi-cercle-extend');
     $('#hidden-text-menu').toggleClass("hidden-for-js");
     $('#hidden-text-menu').toggleClass("show fadeInNew");
     $('.texte').toggleClass("hidden-for-js");
     $('.texte').toggleClass("show");
-    });
+  });
+
+  // $('.cercle-middle').on('hover', function() {
+  //   $(this).toggleClass('box-hover');
 
 
-    $('.js-scrollTo').on('click', function() { // Au clic sur un élément
+
+
+});
+
+$(document).ready(function() {
+
+$('.js-scrollTo').on('click', function() { // Au clic sur un élément
         var page = $(this).attr('href'); // Page cible
         var speed = 750; // Durée de l'animation (en ms)
         $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
         return false;
-      });
+  });
+
 
 
 
 });
 
 
+$(document).ready(function() {
 
+$('.box-test-1').hover(function(){
+    $('.text-box').toggleClass('text-box-hover');
+  });
+
+$('.box-test-1').hover(function(){
+    $('.btn-box').toggleClass('btn-box-hover');
+  });
+
+$('.box-test-1').hover(function(){
+    $(this).toggleClass('fond-noir');
+  });
+});
